94b839ea439871a7b8ea38e1dbe1f87950997333
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2021-12-10 20:03:10 -0600

    heap allocation

--- wrapping_cpp/04_function_depending_on_multiple_cpp_files/test_module/calculator_wrapper.pyx
+++ wrapping_cpp/04_function_depending_on_multiple_cpp_files/test_module/calculator_wrapper.pyx
@@ -18,9 +18,25 @@ cdef extern from "calculator.h" namespace "calc":
     
 class RunCalculator(object):
     def __init__(self, first_inputs, second_inputs, operations):
+        
+       
         results = calculate(first_inputs, second_inputs, operations)
         n = results.size()
         
-        for i in range(n):
-            print(results[i].get_result())
+        # populate class member using return from the c++ function
+        self.result_lst = [results[i].get_result().decode() for i in range(n)]
+        print(self.result_lst)
+        
+        # must to allocate in heap
+        cdef Result *prslt
+        
+        prslt = new Result(4, 5, "subtraction")
+        
+        try:
+            print(prslt.get_result())
+        finally:
+            del prslt
+
+        #for i in range(n):
+        #    print(results[i].get_result())
         
